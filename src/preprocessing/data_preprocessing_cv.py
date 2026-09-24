b6569b89d18fc8786f8a2a6215ffca92625d3733
Jayadeep Narla <103530867+Jayadeep19@users.noreply.github.com>
2026-09-24 13:48:58 +0000

    - can extract the data from cv into desired format.   - TODO: Create webscrapping script to collect all the job postings inthe given company websites.

--- src/preprocessing/data_preprocessing_cv.py
+++ src/preprocessing/data_preprocessing_cv.py
@@ -33,31 +33,40 @@ def read_pdf(path:Path):
         
     except Exception as e:
         print(f"error occurred while reading the pdf file: {e}")
-
+    print(f"Extracted raw text from {path}")
     return text1
 
 def clean_raw_text(text:str) -> str:
     """
-    Clean the raw cv text. (white spaces, symbols)
+    Clean the raw cv text line. (white spaces, symbols)
+    Helper function for clean_text_and_save_csv.
     """
     # normalizing white spaces or tabs
     text = re.sub(r'\s+', ' ', text)
     # broken words across lines
     text = re.sub(r'(\w+)-\s+(\w+)', r'\1-\2', text)
-
-    text = re.sub(r'[^a-zA-Z0-9\+\#\.\,\:\;\-\s]', '', text)
+    text = re.sub(r'&', '', text)
+    #text = re.sub(r'[^a-zA-Z0-9\+\#\.\,\:\;\-\s]', '', text)
     return text.strip()
 
-def save_csv(raw_text, op_path: Path, output_csv_name: str = 'cv_csv.csv'):
+def clean_text_and_save_csv(raw_text, op_path: Path, output_csv_name: str = 'cv_csv.csv'):
     """
     To save the extracted text into csv file.
     """
+
+    # replace the bullet point symbol with a new line character ad split the single sentence into individual lines
+    print("splitting lines from raw text")
+    raw_formatted_text = raw_text.replace('•', '\n')
+    raw_formatted_text = raw_formatted_text.split('\n')
+
+    #Initilize dictionary
     grouped_data = {sec: [] for sec in sections}
 
-    lines = [line.strip() for line in raw_text.split('\n') if line.strip()]
+    lines = [line.strip() for line in raw_formatted_text if line.strip()]
 
     curr_section = "PERSONAL INFO"
 
+    print("cleaning each line")
     for line in lines:
         #clean each line before checking for the section heading
         cleaned_line = clean_raw_text(line)
@@ -79,16 +88,25 @@ def save_csv(raw_text, op_path: Path, output_csv_name: str = 'cv_csv.csv'):
         else:
             if curr_section in grouped_data:
                 grouped_data[curr_section].append(cleaned_line)
-        
+
+    #join all the lines in the professional summary for preserving semantic meaning for the transformer.
+    grouped_data["PROFESSIONAL SUMMARY"] = "".join(value for value in grouped_data["PROFESSIONAL SUMMARY"])
+    print("Grouped the data from raw text into sections and writing the data into csv file")
+
+      
     #write the populated dict into csv file.
-    with open(op_path/output_csv_name, mode = 'w', newline = '', encoding='utf-8') as csv_file:
+    with open(op_path/output_csv_name, mode = 'w', newline = '\n', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['section', 'cleaned_content'])
 
         for sec_name, sec_lines in grouped_data.items():
-            combined_text = ' '.join(sec_lines)
-            writer.writerow([sec_name, combined_text])
-
+            if sec_name == "PROFESSIONAL SUMMARY":
+                combined_text = ''.join(sec_lines)
+                writer.writerow([sec_name, combined_text])
+            else:
+                combined_text = '\n'.join(sec_lines)
+                writer.writerow([sec_name, combined_text])
+    print(f"created csv file at path {op_path/output_csv_name}")
     return grouped_data
 
 if __name__ == "__main__":
@@ -104,6 +122,6 @@ if __name__ == "__main__":
         print(f"CV path doesnot exists")
         sys.exit(1)
     contents = read_pdf(pdf_path)
-    cleaned_string = clean_raw_text(contents)
-
-    save_csv(contents, csv_output_path)
+ 
+    grouped_data = clean_text_and_save_csv(contents, csv_output_path)
+    
